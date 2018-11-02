d1e8d53f9ea86bcca67097f02f338a3ca3497592
giqbal <gohars.iqbal@gmail.com>
2018-11-02 16:03:11 +0000

    Style show info and episode list

--- src/App.js
+++ src/App.js
@@ -18,11 +18,41 @@ class App extends Component {
   render() {
     const {show, episodes, selectedEpisode} = this.state;
     return (
-      <div className="App">
-        <h1>Podcast Player</h1>
-        <img src={show.image_url} alt='Podcast Icon'/>
-        <h2>{show.title}</h2>
-        <p>{show.description}</p>
+      <div>
+        <section className='hero is-dark'>
+          <div className='hero-body'>
+            <div className='container'>
+              <h1 className='title is-1'>Podcast Player</h1>
+            </div>
+          </div>
+        </section>
+        <section className='hero'>
+          <div className='hero-body'>
+            <div className='container level'>
+              <div className='level-left'>
+                <div className='level-item'>
+                  <figure className='image is-128x128'>
+                    <img src={show.image_url} alt='Podcast Icon'/>
+                  </figure>
+                </div>
+                <div className='level-item'>
+                  <h2 className='title is-3'>{show.title}</h2>
+                </div>
+              </div>
+            </div>
+            <div className='level'>
+              <div className='level-left'>
+                <span className='icon'>
+                  <i className='fas fa-quote-left'></i>
+                </span>
+                <p>{show.description}</p>
+                <span className='icon'>
+                  <i className='fas fa-quote-right'></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
         <EpisodeList episodes={episodes} getSelectedEpisode={this.getSelectedEpisode}/>
         <Player episodeToPlay={selectedEpisode} previousOrNextEpisode={this.getEpisode} />
       </div>
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -3,17 +3,28 @@ import moment from 'moment';
 
 const EpisodeList = ({episodes, getSelectedEpisode}) => {
   return (
-    <ul>
-      {episodes.map(episode => 
-      <li key={episode.episode_id} onClick={() => getSelectedEpisode(episode)}>▶
-        <ul>
-          <li>{episode.title}</li>
-          <li>{moment(episode.published_at).format('D MMM YYYY')}</li>
-          <li>{moment.duration(episode.duration).humanize()}</li>
-        </ul>
-      </li>)}
-    </ul>
+    <div className='section'>
+      {episodes.map(episode => {
+        const durationHours = moment.duration(episode.duration).hours();
+        const durationMins = moment.duration(episode.duration).subtract(durationHours, 'h').minutes()
+        return (
+          <div key={episode.episode_id} className='media' onClick={() => getSelectedEpisode(episode)}>
+            <span className='icon media-left'>
+              <i className='fas fa-play-circle'></i>
+            </span>
+            <div className='media-content'>
+              <p>{episode.title}</p>
+              <p>{moment(episode.published_at).format('D MMM YYYY')}</p>
+            </div>
+            <div className='media-right'>
+              <p>{`${durationHours} hrs ${durationMins} min`}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
+  
 };
 
 export default EpisodeList;
