7af60d27259a097b7393afd14d0438ab1b908e34
giqbal <gohars.iqbal@gmail.com>
2018-11-04 22:49:43 +0000

    Styled Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,18 +6,37 @@ class Player extends Component {
     const {episodeToPlay, previousOrNextEpisode} = this.props;
     return (
       episodeToPlay?
-        <div>
-          <p>{episodeToPlay.title}</p>
-          <img src={episodeToPlay.image_url} alt='Episode artwork'/>
-          <span onClick={() => previousOrNextEpisode('previous')}>⏮</span>
-          <ReactPlayer url={`https://api.spreaker.com/v2/episodes/${episodeToPlay.episode_id}/play`} config={
-            {
-              file: {
-                forceAudio: true
-              }
-            }
-          } controls/>
-          <span onClick={() => previousOrNextEpisode('next')}>⏭</span>
+        <div className='navbar is-fixed-bottom is-black'>
+        <div className='navbar-brand'>
+          <figure className='image is-48x48'>
+            <img className='is-rounded' src={episodeToPlay.image_url} alt='Episode artwork'/>
+          </figure>
+        
+          {/* <div className='navbar-menu'> */}
+            {/* <div className='navbar-start'> */}
+              <span className='navbar-item' onClick={() => previousOrNextEpisode('previous')}>
+                <i className='fas fa-backward'></i>
+              </span>
+              <span className='navbar-item' onClick={() => previousOrNextEpisode('next')}>
+              <i className='fas fa-forward'></i>
+              </span>
+            {/* </div> */}
+            </div>
+            <ReactPlayer 
+                url={`https://api.spreaker.com/v2/episodes/${episodeToPlay.episode_id}/play`}
+                config={
+                  {
+                    file: {
+                      forceAudio: true
+                    }
+                  }
+                }
+                controls={true}
+                width='100%'
+                height='100%'
+              />
+              
+          {/* </div> */}
         </div>
         :
         <p>Select episode to play</p>
