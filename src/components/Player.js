66c42895505bb93f612087c30bb84e148a0911eb
giqbal <gohars.iqbal@gmail.com>
2018-11-04 22:54:12 +0000

    Clean up Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,40 +6,34 @@ class Player extends Component {
     const {episodeToPlay, previousOrNextEpisode} = this.props;
     return (
       episodeToPlay?
-        <div className='navbar is-fixed-bottom is-black'>
+      <div className='navbar is-fixed-bottom is-black'>
         <div className='navbar-brand'>
           <figure className='image is-48x48'>
             <img className='is-rounded' src={episodeToPlay.image_url} alt='Episode artwork'/>
           </figure>
-        
-          {/* <div className='navbar-menu'> */}
-            {/* <div className='navbar-start'> */}
-              <span className='navbar-item' onClick={() => previousOrNextEpisode('previous')}>
-                <i className='fas fa-backward'></i>
-              </span>
-              <span className='navbar-item' onClick={() => previousOrNextEpisode('next')}>
-              <i className='fas fa-forward'></i>
-              </span>
-            {/* </div> */}
-            </div>
-            <ReactPlayer 
-                url={`https://api.spreaker.com/v2/episodes/${episodeToPlay.episode_id}/play`}
-                config={
-                  {
-                    file: {
-                      forceAudio: true
-                    }
-                  }
-                }
-                controls={true}
-                width='100%'
-                height='100%'
-              />
-              
-          {/* </div> */}
+          <span className='navbar-item' onClick={() => previousOrNextEpisode('previous')}>
+              <i className='fas fa-backward'></i>
+          </span>
+          <span className='navbar-item' onClick={() => previousOrNextEpisode('next')}>
+            <i className='fas fa-forward'></i>
+          </span>
         </div>
-        :
-        <p>Select episode to play</p>
+        <ReactPlayer 
+          url={`https://api.spreaker.com/v2/episodes/${episodeToPlay.episode_id}/play`}
+          config={
+            {
+              file: {
+                forceAudio: true
+              }
+            }
+          }
+          controls={true}
+          width='100%'
+          height='100%'
+        />
+      </div>
+      :
+      <p>Select episode to play</p>
     );
   }
 }
